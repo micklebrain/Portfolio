271e28a48e63aee34ad8b005db1424ab3338f8e8
white-rose <Cabinboy23>
2018-03-31 14:46:53 -0700

    Download Resume PDF added

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -41,3 +41,12 @@ console.log($hackLists);
 $window.on('scroll resize', check_if_in_view);
 $window.trigger('scroll');
 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element)
+}
+
