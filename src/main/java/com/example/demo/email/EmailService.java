a0c7c298a946d16dcefa46d3305dddbffaa2842b
Nathan Nguyen <nathanthainguyen@gmail.com>
2018-05-09 14:17:06 -0700

    test patch

--- src/main/java/com/example/demo/email/EmailService.java
+++ src/main/java/com/example/demo/email/EmailService.java
@@ -15,23 +15,23 @@ import org.springframework.web.servlet.view.RedirectView;
 @RestController
 public class EmailService {
 
-    @Autowired
-    public JavaMailSender emailSender;
-
-    @RequestMapping(
-            value = "/send",
-            method = RequestMethod.POST
-    )
-    public RedirectView sendEmail(@ModelAttribute Email email) {
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo("nathanthainguyen@gmail.com");
-        message.setSubject(email.getSubject());
-        message.setText(email.getContent());
-
-        emailSender.send(message);
-
-        return new RedirectView("/");
-    }
+//    @Autowired
+//    public JavaMailSender emailSender;
+//
+//    @RequestMapping(
+//            value = "/send",
+//            method = RequestMethod.POST
+//    )
+//    public RedirectView sendEmail(@ModelAttribute Email email) {
+//
+//        SimpleMailMessage message = new SimpleMailMessage();
+//        message.setTo("nathanthainguyen@gmail.com");
+//        message.setSubject(email.getSubject());
+//        message.setText(email.getContent());
+//
+//        emailSender.send(message);
+//
+//        return new RedirectView("/");
+//    }
 
 }
